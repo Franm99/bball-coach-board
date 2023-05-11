3836fa7f79778b2d7836901bf219494581aca338
Fran Moreno <fran.moreno.se@gmail.com>
2023-05-11 11:15:21 +0200

    Working with two different boards instead of one

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 /*
-TODO: Keep working on paths: 
-    - paths could shade so it's clear what's the most recent movement
-TODO: Keep working on button to start play:
-    - With JS, make button to keep pressed and change to Stop play when pressed again
-TODO: Add more options for default positions:
-    - Define "plays", create some of the basic ones.
-TODO: Add Undo button (and give the same functionality to Ctrl+Z)
-TODO: Save last state when changing court (so when going back, the positions are the same as before)
+TODO Investigate about shading paths. 
+TODO Think about some sort of structure to handle path features, like color, type of line or width.
+TODO Define "plays".
 */
 
 const UNDO_LIMIT = 10;
@@ -195,16 +190,19 @@ class Board {
     /**
      * Class constructor.
      */
-    constructor() {
+    constructor(display) {
         // Canvas related properties
         this.canvas_back = document.getElementById('canvas1');
         this.context_back = this.canvas_back.getContext('2d');
         this.canvas_front = document.getElementById('canvas2');
         this.context_front = this.canvas_front.getContext('2d');
 
-        this.width;
-        this.height;
         this.startPos = [];
+
+        this.display = display;
+        [this.backgroundImage, this.resizeRatio] = this.select_background_image();
+        this.width = this.backgroundImage.width * this.resizeRatio;
+        this.height = this.backgroundImage.height * this.resizeRatio;
 
         // Elements for first canvas
         this.ball;
@@ -221,106 +219,120 @@ class Board {
         // Parameters to draw lines
         this.startPlay = false;
 
-        this.setup();
-
-        // Register Event Listeners
-        this.canvas_front.addEventListener('mousedown', e=> {
-            /* Dragging object. */
-            e.preventDefault();
-            this.startPos = [parseInt(e.offsetX), parseInt(e.offsetY)];
-
-            this.objects.forEach(object => {
-                if (this.is_mouse_in_object(this.startPos, object)){
-
-                    this.add_action(object);
-                    this.dragging_object = true;
-                    this.current_action.path.push(this.startPos);
-                    return;                      
-                }
-            });
-        });
-
-        this.canvas_front.addEventListener('mouseup', e=> {
-            /* Dropping object. */
-            if (!this.dragging_object) { return; }
-            e.preventDefault();
-            this.dragging_object = false;
-        });
-
-        this.canvas_front.addEventListener('mouseout', e=> {
-            /* Mouse out of canvas, drop object. */
-            if (!this.dragging_object) { return; }
-            e.preventDefault();
-        });
-
-        this.canvas_front.addEventListener('mousemove', e=> {
-            /* Move object when dragged. */
-            if (!this.dragging_object) {
-                return;
-            }
-            else {
-                e.preventDefault();
-
-                let mousePos = [parseInt(e.offsetX), parseInt(e.offsetY)];
-                let distance = [mousePos[0] - this.startPos[0], mousePos[1] - this.startPos[1]];
-
-                // Move object
-                this.current_action.object.pos[0] += distance[0];
-                this.current_action.object.pos[1] += distance[1];
-
-                this.current_action.path.push(mousePos);
-
-                if (this.startPlay) {
-                    this.draw_path(this.startPos, mousePos);
-                }
-                this.draw_objects();
-
-                this.startPos = mousePos;
-            }
-
-        });
-    }
-
-    /**
-     * Initializes the canvas depending on what display is desired: Full or Half court.
-     * @param {Number} [Optional] display - Choose from COURT Enum.
-     */
-    setup(display=COURT.half) {
-        let im_path;
-        let image = new Image();
-        let resize_ratio;
-
-        if (display == COURT.half){
-            im_path = 'img/half-court.jpg';
-            resize_ratio = 0.2;
-        } else {
-            im_path = 'img/full-court.jpg';
-            resize_ratio = 0.1;
-        }
-
-        image.src = im_path;
-
-        // First canvas
-        this.canvas_back.style.background = `url('${im_path}')`;
-        this.canvas_back.style.backgroundSize = "auto 100%";
-        this.canvas_back.style.backgroundRepeat = "no-repeat";
-        this.canvas_back.width = image.width * resize_ratio;
-        this.canvas_back.height = image.height * resize_ratio;
-
-        // Second canvas (same size)
-        this.canvas_front.width = image.width * resize_ratio;
-        this.canvas_front.height = image.height * resize_ratio;
-
-        this.width = this.canvas_back.width;
-        this.height = this.canvas_back.height;
+        this.isCurrentBoard = false;
 
         // Objects
         this.team_guest = new Team(TEAM.guest, [this.width - 45, 10]);
         this.team_home = new Team(TEAM.home, [10, 10]);
         this.ball = new Ball([this.width / 2, this.height / 2], 15);
         this.objects = [].concat(this.team_guest.players, this.team_home.players, this.ball);
-   
+
         this.draw_objects();  // Initial draw
+
+        // this.setup();
+
+        // Register Event Listeners
+        this.canvas_front.addEventListener('mousedown', e=> {
+            /* Dragging object. */
+            if (this.isCurrentBoard) {
+                e.preventDefault();
+                this.startPos = [parseInt(e.offsetX), parseInt(e.offsetY)];
+    
+                this.objects.forEach(object => {
+                    if (this.is_mouse_in_object(this.startPos, object)){
+    
+                        this.add_action(object);
+                        this.dragging_object = true;
+                        this.current_action.path.push(this.startPos);
+                        return;                      
+                    }
+                });
+            }
+        });
+
+        this.canvas_front.addEventListener('mouseup', e=> {
+            /* Dropping object. */
+            if (this.isCurrentBoard) {
+                if (!this.dragging_object) { return; }
+                e.preventDefault();
+                this.dragging_object = false;
+            }
+        });
+
+        this.canvas_front.addEventListener('mouseout', e=> {
+            /* Mouse out of canvas, drop object. */
+            if (this.isCurrentBoard) {
+                if (!this.dragging_object) { return; }
+                e.preventDefault();
+            }
+        });
+
+        this.canvas_front.addEventListener('mousemove', e=> {
+            /* Move object when dragged. */
+            if (this.isCurrentBoard) {
+                if (!this.dragging_object) {
+                    return;
+                }
+                else {
+                    e.preventDefault();
+    
+                    let mousePos = [parseInt(e.offsetX), parseInt(e.offsetY)];
+                    let distance = [mousePos[0] - this.startPos[0], mousePos[1] - this.startPos[1]];
+    
+                    // Move object
+                    this.current_action.object.pos[0] += distance[0];
+                    this.current_action.object.pos[1] += distance[1];
+    
+                    this.current_action.path.push(mousePos);
+    
+                    if (this.startPlay) {
+                        this.draw_path(this.startPos, mousePos);
+                    }
+                    this.draw_objects();
+    
+                    this.startPos = mousePos;
+                }
+            }
+        });
+    }
+
+    /**
+     * Set as current board depending on display type.
+     */
+    setup() {
+        this.isCurrentBoard = true;
+
+        // First canvas
+        this.canvas_back.style.background = `url('${this.backgroundImage.src}')`;
+        this.canvas_back.style.backgroundSize = "auto 100%";
+        this.canvas_back.style.backgroundRepeat = "no-repeat";
+        this.canvas_back.width = this.width;
+        this.canvas_back.height = this.height;
+
+        // Second canvas (same size)
+        this.canvas_front.width = this.width;
+        this.canvas_front.height = this.height;
+
+        this.width = this.canvas_back.width;
+        this.height = this.canvas_back.height;
+
+        this.draw_objects();
+        this.draw_play();
+        return this;
+    }
+
+    select_background_image() {
+        let image = new Image();
+        let resizeRatio;
+
+        if (this.display == COURT.half) {
+            image.src = 'img/half-court.jpg';
+            resizeRatio = 0.2;
+        } else {
+            image.src = 'img/full-court.jpg';
+            resizeRatio = 0.1;
+        }
+        return [image, resizeRatio];
     }
 
     /**
@@ -334,6 +346,23 @@ class Board {
                 object.draw(this.context_front);
             }
         });
+    }
+
+    draw_play() {
+        // Draw paths if Play mode is On.
+        if (this.startPlay) {
+            this.actions.forEach(action => {
+                if ( action.onPlay) {
+                    let segment = action.path;
+                    let init_point = segment.shift();
+                    segment.forEach(point => {
+                        // TODO: seems to remove some initial/last point from path when redrawing. Solve it.
+                        this.draw_path(init_point, point, action.object.path_color);
+                        init_point = point;
+                    });
+                    }
+            });
+        }
     }
 
     /**
@@ -352,7 +381,7 @@ class Board {
     }
 
     undo_action() {
-        // todo: Too much things happening here. Refactor a bit.
+        // TODO: Too much things happening here. Refactor a bit.
 
         if (this.actions.length == 0) {
             return;  // No actions can be reversed.
@@ -365,21 +394,7 @@ class Board {
         // Move current object to new position
         this.current_action.object.undo_move();
         this.draw_objects();
-
-        // Draw paths if Play mode is On.
-        if (this.startPlay) {
-            this.actions.forEach(action => {
-                if ( action.onPlay) {
-                    let segment = action.path;
-                    let init_point = segment.shift();
-                    segment.forEach(point => {
-                        // todo: seems to remove some initial/last point from path when redrawing. Solve it.
-                        this.draw_path(init_point, point, action.object.path_color);
-                        init_point = point;
-                    });
-                 }
-            });
-        }
+        this.draw_play();
 
         // Update action to last registered
         this.current_action = this.actions[this.actions.length - 1];
@@ -426,6 +441,7 @@ class Board {
         });
 
         this.startPlay = false; // Stop drawing paths.
+        this.actions = [];
     }
 }
 
@@ -437,57 +453,79 @@ window.addEventListener('load', function(){
     const buttonReset = document.getElementById('buttonReset');
     const checkboxHome = document.getElementById('homeTeam');
     const checkboxGuest = document.getElementById('guestTeam');
-    const buttonPlay = document.getElementById('buttonPlay');
+    const checkboxPlay = this.document.getElementById('drawPaths');
+    const textPlay = this.document.getElementById('textPlay');
     const buttonUndo = this.document.getElementById('buttonUndo');
 
     // Initialize board
-    let board = new Board();
-
+    // let board = new Board(); ///
+    let boardHalf = new Board(display=COURT.half);
+    let boardFull = new Board(display=COURT.full);
+    let currentBoard = boardHalf.setup();
     // Event listeners for HTML elements.
-    
+
     toggleSwitch.addEventListener('change', function() {
         if (!this.checked) {
-            board.setup(COURT.half);
+            currentBoard = boardHalf.setup();
+            boardFull.isCurrentBoard = false;
         } else {
-            board.setup(COURT.full);
+            currentBoard = boardFull.setup();
+            boardHalf.isCurrentBoard = false;
         }
 
         // By default, both teams will be shown when a board is initialized.
+        // FIXME save state when switching boards.
         checkboxHome.checked = true;
         checkboxGuest.checked = true;
 
-        board.startPlay = false;
+        if (currentBoard.startPlay){
+            checkboxPlay.checked = true;
+            textPlay.innerHTML = "Stop Play"
+        } else {
+            checkboxPlay.checked = false;
+            textPlay.innerHTML = "Start Play"
+        }
+
     });
 
     buttonReset.addEventListener('click', function(){
-        board.reset();
+        currentBoard.reset();
+        checkboxPlay.checked = false;
+        textPlay.innerHTML = "Start Play"
     });
 
     checkboxHome.addEventListener('change', function() {
         if (!this.checked) {
-            board.team_home.show(false);
+            currentBoard.team_home.show(false);
         } else {
-            board.team_home.show(true);
+            currentBoard.team_home.show(true);
         }
-        board.draw_objects();
+        currentBoard.draw_objects();
     });
 
     
     checkboxGuest.addEventListener('change', function() {
         if (!this.checked) {
-            board.team_guest.show(false);
+            currentBoard.team_guest.show(false);
         } else {
-            board.team_guest.show(true);
+            currentBoard.team_guest.show(true);
         }
-        board.draw_objects();
+        currentBoard.draw_objects();
     });
 
-    buttonPlay.addEventListener('click', function(){
-        board.startPlay = true;
+    checkboxPlay.addEventListener('change', function() {
+        if (!this.checked) {
+            currentBoard.startPlay = false;
+            textPlay.innerHTML = "Start Play";
+        } else {
+            currentBoard.startPlay = true;
+            textPlay.innerHTML = "Stop Play";
+        }
     });
 
     buttonUndo.addEventListener('click', function(){
-        // todo: If no actions on board, disable button.
-        board.undo_action();
+        // TODO: If no actions on board, disable button.
+        currentBoard.undo_action();
     });
-})
+
+});
